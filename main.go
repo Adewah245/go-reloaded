315e60cb37f6fe17ff4537d7fc211803544e89f2
Adewah245 <adewahjames@gmail.com>
2026-03-31 14:07:02 +0100

    my very first commit

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	// hex to decimal
-	fmt.Println(hex("ff"))
-	fmt.Println(hex("1E"))
-	// bin to decimal
-	fmt.Println(bin("1010"))
-	fmt.Println(bin("111111"))
+	// 	// hex to decimal
+	// 	fmt.Println(hex("ff"))
+	// 	fmt.Println(hex("1E"))
+
+	// 	// bin to decimal
+	// 	fmt.Println(bin("1010"))
+	// 	fmt.Println(bin("111111"))
+	// 	// change A to An
+	fmt.Println(changeA([]string{"there it was.", "a amazing rock.", "a honest man.", "a book"}))
 }
